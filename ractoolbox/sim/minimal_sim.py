bcba442f63056a646ba6b0916a804cc17985453b
Derek Fan <derekfan.18.2002@gmail.com>
2023-10-24 21:46:35 -0700

    introduced sim timestamp

--- ractoolbox/sim/minimal_sim.py
+++ ractoolbox/sim/minimal_sim.py
@@ -19,7 +19,8 @@ class MinimalSim():
         sprite_size=1.0,
     ) -> None:
         self._assert(
-            backend, controller, lb_pose, ub_pose, data_len, sprite_size)
+            backend, controller, lb_pose, ub_pose, data_len, sprite_size,)
+        self._fig, self._ax, self._text = self._init_fig()
         self._backend = backend
         self._controller = controller
         self._xlim = (lb_pose[0], ub_pose[0])
@@ -31,8 +32,15 @@ class MinimalSim():
         self._p3 = np.array([0, sprite_size / 2, 0, 1])
         self._p4 = np.array([0, -sprite_size / 2, 0, 1])
 
-        self._fig, self._ax = self._init_fig()
         self._run_flag = mp.Value("b", False)
+        self._verbose = mp.Value("b", False)
+        self._pose_data = np.zeros((data_len, 3))
+        self._data_ind = 0
+        self._x = mp.Array("f", np.zeros(backend.nx))
+        self._x_set = mp.Array("f", np.zeros(backend.nx))
+        self._max_steps = mp.Value("i", -1)
+        self._steps = mp.Value("i", 0)
+        self._sim_time = mp.Value("f", 0.0)
 
 
     @property
@@ -44,6 +52,7 @@ class MinimalSim():
         self,
         x0: np.ndarray,
         max_steps=-1,
+        verbose=False,
     ) -> None:
         if self._run_flag.value:
             raise RuntimeError(
@@ -51,8 +60,9 @@ class MinimalSim():
         else:
             assert len(x0) == self._backend.nx
             assert type(max_steps) == int
-            self._init_vars(x0, max_steps)
-            print("Starting simulator...")
+            assert type(verbose) == bool
+            self._reset(x0, max_steps, verbose)
+            print("\nStarting simulator...")
             manager_proc = mp.Process(target=self._run_procs, args=[])
             manager_proc.start()
 
@@ -73,19 +83,21 @@ class MinimalSim():
                 "You cannot assign a setpoint before the sim starts.")
 
 
-    def _init_vars(
+    def _reset(
         self,
         x0: np.ndarray,
-        max_steps: int
+        max_steps: int,
+        verbose: bool,
     ) -> None:
-        self._pose_data = np.empty((self._data_len, 3))
-        self._data_ind = 0
-        self._x = mp.Array("f", x0)
-        self._x_set = mp.Array("f", x0)
-        self._max_steps = mp.Value("i", max_steps)
-        self._steps = mp.Value("i", 0)
-        self._sim_time = mp.Value("f", 0.0)
         self._run_flag.value = True
+        self._verbose.value = verbose
+        self._pose_data = np.zeros((self._data_len, 3))
+        self._data_ind = 0
+        self._x[:] = x0
+        self._x_set[:] = x0
+        self._max_steps.value = max_steps
+        self._steps.value = 0
+        self._sim_time.value = 0.0
 
 
     def _run_procs(self) -> None:
@@ -95,34 +107,31 @@ class MinimalSim():
         backend_proc.start()
         backend_proc.join()
         frontend_proc.join()
-        print("\nSimulator successfully exited.")
-        #self._frontend_proc.start()
-        #self._backend_proc.start()
-        #if self._max_steps.value > 0:
-        #    self._join_procs()
+        print("\nSimulator successfully closed.")
 
 
     def _run_frontend(self) -> None:
         plt.ion()
         while self._run_flag.value:
-            self._plot(self._x[:], timer=True)
+            self._plot(self._x[:], timer=self._verbose.value)
         plt.cla()
         plt.ioff()
-        #plt.close()
+        plt.close()
 
 
     def _plot(
         self,
         x: np.ndarray,
-        timer=False,
+        timer: bool,
     ) -> None:
         st = time.perf_counter()
         self._update_data(x)
+        self._update_time()
         p1, p2, p3, p4 = self._transform_quad(x)
-        plt.cla()
         self._plot_quad(p1, p2, p3, p4)
-        self._set_plot_settings()
-        if timer: print(f"render runtime: {time.perf_counter() - st}")
+        self._set_plot()
+        if timer:
+            print(f"render runtime: {time.perf_counter() - st}")
 
 
     def _update_data(
@@ -142,10 +151,16 @@ class MinimalSim():
                 self._data_ind += 1
 
 
+    def _update_time(self) -> None:
+        sim_time = "{:.2f}".format(round(self._sim_time.value, 2))
+        timestamp = f"simulator time: {sim_time} s"
+        self._text.set_text(timestamp)
+
+
     def _transform_quad(
-            self,
-            x: np.ndarray,
-        ) -> Tuple[float]:
+        self,
+        x: np.ndarray,
+    ) -> Tuple[float]:
             T = self._get_transform(x)
             p1_t = T @ self._p1
             p2_t = T @ self._p2
@@ -185,6 +200,7 @@ class MinimalSim():
         p3: float,
         p4: float,
     ) -> None:
+        plt.cla()
         self._ax.plot([p1[0], p2[0], p3[0], p4[0]],
                      [p1[1], p2[1], p3[1], p4[1]],
                      [p1[2], p2[2], p3[2], p4[2]], "k.", markersize=9)
@@ -194,12 +210,9 @@ class MinimalSim():
                      [p3[2], p4[2]], "b-", linewidth=3)
         self._ax.plot(self._pose_data[:,0], self._pose_data[:,1],
                      self._pose_data[:,2], "r.", markersize=1)
-        timestamp = round(self._sim_time.value, 4)
-        #plt.text(0.02, 0.5, timestamp, fontsize=14, transform=plt.gcf().transFigure)
-        plt.gcf().text(0.02, 0.5, str(timestamp), fontsize=14)
 
 
-    def _set_plot_settings(self) -> None:
+    def _set_plot(self) -> None:
         self._ax.set_xlim(self._xlim)
         self._ax.set_ylim(self._ylim)
         self._ax.set_zlim(self._zlim)
@@ -207,11 +220,11 @@ class MinimalSim():
         self._ax.set_yticks(range(self._ylim[0], self._ylim[1], 2))
         self._ax.set_zticks(range(self._zlim[0], self._zlim[1]+2, 2))
         self._ax.xaxis.set_rotate_label(False)
-        self._ax.set_xlabel(r"$\bf{x}$", fontsize=15)
+        self._ax.set_xlabel(r"$\bf{x}$", fontsize=12)
         self._ax.yaxis.set_rotate_label(False)
-        self._ax.set_ylabel(r"$\bf{y}$", fontsize=15)
+        self._ax.set_ylabel(r"$\bf{y}$", fontsize=12)
         self._ax.zaxis.set_rotate_label(False)
-        self._ax.set_zlabel(r"$\bf{z}$", fontsize=15,)
+        self._ax.set_zlabel(r"$\bf{z}$", fontsize=12)
         plt.pause(0.00001)
 
 
@@ -219,27 +232,29 @@ class MinimalSim():
         while self._run_flag.value:
             x0 = np.array(self._x[:])
             x_set = np.array(self._x_set[:])
-            u = self._controller.get_input(x0=x0, x_set=x_set, timer=True)
-            x = self._backend.update(x0=x0, u=u, timer=True)
+            u = self._controller.get_input(x0=x0, x_set=x_set, timer=self._verbose.value)
+            x = self._backend.update(x0=x0, u=u, timer=self._verbose.value)
             self._x[:] = x
-            print(f"u: {u}")
-            print(f"x: {x}")
-            self._update_time()
+            if self._verbose.value:
+                print(f"u: {u}")
+                print(f"x: {x}")
+            self._check_steps()
 
 
-    def _update_time(self):
+    def _check_steps(self):
         self._steps.value += 1
         self._sim_time.value += self._controller.dt
         if self._max_steps.value > 0 and self._steps.value == self._max_steps.value:
             self._run_flag.value = False
 
 
-    def _init_fig(self) -> Tuple[matplotlib.figure.Figure, plt.Axes]:
+    def _init_fig(self) -> Tuple[matplotlib.figure.Figure, plt.Axes, matplotlib.text.Text]:
         fig = plt.figure(figsize=(9,10))
         fig.canvas.mpl_connect("key_release_event",
             lambda event: [exit(0) if event.key == "escape" else None])
-        ax = fig.add_subplot(projection="3d")
-        return fig, ax
+        ax = fig.gca(projection="3d")
+        text = plt.gcf().text(0.6, 0.85, "0.00", fontsize=14)
+        return fig, ax, text
 
 
     def _assert(
@@ -284,8 +299,8 @@ class MinimalSim():
             raise ValueError(
                 "Please input an upper bound on position as a 3D vector!")
         if type(data_len) != int:
-            raise ValueError(
+            raise TypeError(
                 "Please input the desired data length as an integer!")
         if (type(sprite_size) != int and type(sprite_size) != float):
-            raise ValueError(
+            raise TypeError(
                 "Please input the desired sprite size as an integer or float!")
