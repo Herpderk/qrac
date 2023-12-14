72ece6652d8152c76b046abcffacc1ce51a04e25
Derek Fan <derekfan.18.2002@gmail.com>
2023-12-13 16:58:39 -0800

    refactored circular trajectory into a class

--- qrac/trajectory.py
+++ qrac/trajectory.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 
 import casadi as cs
+import numpy as np
 
 
-def circle(r: float, alt: float):
-    t = cs.SX.sym("t")
-    pos_ang = cs.SX(cs.vertcat(
-        r*cs.cos(t),
-        r*cs.sin(t),
-        alt,
-        cs.SX.zeros(3,1)))
-    vels = cs.SX(cs.jacobian(pos_ang, t))
-    traj = cs.SX(cs.vertcat(pos_ang, vels))
-    traj = cs.reshape(traj, 1, 12)
-    return cs.Function("trajectory", [t], [traj])
+class Circle():
+    def __init__(self, r: float, alt: float):
+        t = cs.SX.sym("t")
+        traj = cs.SX.sym("traj", 12)
+        pos_ang = cs.SX(cs.vertcat(
+            r*cs.cos(t),
+            r*cs.sin(t),
+            alt,
+            cs.SX.zeros(3,1)))
+        vels = cs.SX(cs.jacobian(pos_ang, t))
+
+        traj[0:6] = pos_ang
+        traj[6:12] = vels
+        self._traj = cs.Function("trajectory", [t], [traj])
+
+    def get_setpoint(self, t: float):
+        setpt = self._traj(t)
+        return np.array(setpt).flatten()
