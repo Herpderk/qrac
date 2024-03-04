32feed39d47cc788bd36748cac21a48070f336a3
Derek Fan <derekfan.18.2002@gmail.com>
2024-03-03 18:43:39 -0800

    separated set membership from parameter adaptive mpc

---
+++ qrac/control/param_nmpc.py
@@ -0,0 +1,143 @@
+#!/usr/bin/python3
+
+import numpy as np
+import multiprocessing as mp
+from scipy.linalg import block_diag
+from qrac.models import Quadrotor, ParameterAffineQuadrotor
+from qrac.control.nmpc import NMPC
+
+
+def npify(arr_like) -> np.ndarray:
+    return np.array(arr_like[:])
+
+
+class ParameterAdaptiveNMPC():
+    def __init__(
+        self,
+        estimator,
+        model: Quadrotor,
+        Q: np.ndarray,
+        R: np.ndarray,
+        u_min: np.ndarray,
+        u_max: np.ndarray,
+        time_step: float,
+        num_nodes: int,
+        rti: bool,
+        real_time: bool,
+        nlp_tol=10**-6,
+        nlp_max_iter=10,
+        qp_max_iter=10,
+    ) -> None:
+        self._nx = model.nx
+        self._nu = model.nu
+        self._N = num_nodes
+        self._rt = real_time
+
+        model_aug = ParameterAffineQuadrotor(model)
+        self._nparam = model_aug.nparam
+        Q_aug = self._augment_costs(Q)
+        self._mpc = NMPC(
+            model=model_aug, Q=Q_aug, R=R,
+            u_min=u_min, u_max=u_max,
+            time_step=time_step,
+            num_nodes=num_nodes, rti=rti,
+            nlp_tol=nlp_tol, nlp_max_iter=nlp_max_iter,
+            qp_max_iter=qp_max_iter
+        )
+        self._param_est = estimator
+
+        self._param = mp.Array("f", model_aug.get_parameters())
+        self._x = mp.Array("f", np.zeros(model.nx))
+        self._xprev = mp.Array("f", np.zeros(self._nx))
+        self._uprev = mp.Array("f", np.zeros(self._nu))
+        self._timer = False
+        if real_time:
+            self._run_flag = mp.Value("b", True)
+
+
+    @property
+    def dt(self) -> float:
+        return self._mpc.dt
+
+
+    @property
+    def n_set(self) -> int:
+        return self._N * self._nx
+
+
+    def start(self) -> None:
+        if not self._rt:
+            print("This controller is not in real-time mode!")
+        else:
+            proc = mp.Process(target=self._param_proc, args=[])
+            proc.start()
+
+
+    def stop(self) -> None:
+        if not self._rt:
+            print("This controller is not in real-time mode!")
+        else:
+            self._run_flag.value = False
+
+
+    def get_input(
+        self,
+        x: np.ndarray,
+        xset: np.ndarray,
+        timer=False,
+    ) -> np.ndarray:
+        self._xprev[:] = self._x[:]
+        self._x[:] = x
+        self._timer.value = timer
+        if not self._rt: self._get_param()
+
+        x_aug = np.concatenate((x, npify(self._param)))
+        xset_aug = self._augment_xset(xset)
+        self._uprev[:] = self._mpc.get_input(x_aug, xset_aug, timer)
+        return npify(self._uprev)
+
+
+    def _augment_xset(
+        self,
+        xset: np.ndarray
+    ) -> np.ndarray:
+        nx = self._nx
+        nparam = self._nparam
+        xset_aug = np.zeros(self._N * (nx+nparam))
+        for k in range(self._N):
+            xset_aug[k*(nx+nparam) : k*(nx+nparam) + nx] =\
+                xset[k*nx : k*nx + nx]
+        return xset_aug
+
+
+    def _param_proc(self) -> None:
+        param_proc = mp.Process(target=self._run_param_est)
+        param_proc.start()
+        param_proc.join()
+        print("\nParameter Estimator successfully stopped.")
+
+
+    def _run_param_est(self) -> None:
+        while self._run_flag.value:
+            self._get_param()
+
+
+    def _get_param(self) -> None:
+        param = self._param_est.get_param(
+            x=npify(self._x),
+            xprev=npify(self._xprev),
+            uprev=npify(self._uprev),
+            param=npify(self._param),
+            timer=self._timer
+        )
+        self._param[:] = param
+
+
+    def _augment_costs(
+        self,
+        Q: np.ndarray,
+    ) -> np.ndarray:
+        Q_aug = block_diag(
+            Q, np.zeros((self._nparam, self._nparam))
+        )
+        return Q_aug
