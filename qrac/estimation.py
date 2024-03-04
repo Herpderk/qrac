6a338e01d27275adb36f9eb7fa52801950d2f190
Derek Fan <derekfan.18.2002@gmail.com>
2024-03-03 22:00:54 -0800

    renamed

--- qrac/estimation.py
+++ qrac/estimation.py
@@ -17,24 +17,24 @@ class SetMembershipEstimator:
         disturb_min: np.ndarray,
         disturb_max: np.ndarray,
         time_step: float,
-        qp_tol: float,
-        max_iter: int,
+        qp_tol=10**-6,
+        max_iter=10,
     ) -> None:
         self._nx = model.nx
-        self._param_est = estimator
+        self._est = estimator
         model_aug = ParameterAffineQuadrotor(model)
         self._Fd, self._Gd, self._Gd_T = \
             self._get_discrete_dynamics(model_aug, self._nx, time_step)
 
-        self._nparam = model_aug.nparam
+        self._np = model_aug.np
         self._d_min = disturb_min
         self._d_max = disturb_max
-        self._param_tol = param_tol
-        self._solver_tol = qp_tol
+        self._p_tol = param_tol
+        self._sol_tol = qp_tol
         self._max_iter = max_iter
 
-        self._param_min = param_min
-        self._param_max = param_max
+        self._p_min = param_min
+        self._p_max = param_max
         self._start = False
 
 
@@ -61,18 +61,15 @@ class SetMembershipEstimator:
         timer=False
     ) -> np.ndarray:
         st = time.perf_counter()
-        est_param = self._param_est.get_param(x, xprev, uprev, param, False)
-        param_min, param_max = self._sm_update(x, xprev, uprev)
-        param_proj = self._solve_proj(
-            param=est_param, param_min=param_min, param_max=param_max,
-            tol=self._solver_tol, max_iter=self._max_iter
-        )
-        self._update_param_bds(param_proj, param_min, param_max)
-        print(f"params: {param_proj}")
+        p_min, p_max = self._sm_update(x, xprev, uprev)
+        self._update_param_bds(p_min, p_max)
+        p_est = self._est.get_param(x, xprev, uprev, param, False)
+        p_proj = self._solve_proj(param=p_est)
+        print(f"params: {p_proj}")
         if timer:
             et = time.perf_counter()
-            print(f"SetMembership and {type(self._param_est)} runtime: {et - st}")
-        return param_proj
+            print(f"sm runtime: {et - st}")
+        return p_proj
 
 
     def _sm_update(
@@ -85,34 +82,28 @@ class SetMembershipEstimator:
             self._start = True
             xprev = x
 
-        param_min = self._param_min
-        param_max = self._param_max
-        if not (param_max - param_min < self._tol).all():
-            sol_min = np.zeros(self._nparam)
-            sol_max = np.zeros(self._nparam)
+        p_min = self._p_min
+        p_max = self._p_max
+        if not (p_max - p_min < self._p_tol).all():
+            sol_min = np.zeros(self._np)
+            sol_max = np.zeros(self._np)
 
-            for i in range(self._nparam):
-                if param_max[i] - param_min[i] > self._tol[i]:
+            for i in range(self._np):
+                if p_max[i] - p_min[i] > self._p_tol[i]:
                     sol_min[i] = self._solve_lp(
-                        idx=i, x=x, xprev=xprev, uprev=uprev,
-                        d_min=self._d_min, d_max=self._d_max,
-                        param_min=param_min, param_max=param_max,
-                        tol=self._solver_tol, max_iter=self._max_iter,
-                        max=False
+                        idx=i, x=x, xprev=xprev,
+                        uprev=uprev, max=False
                     )
                     sol_max[i] = self._solve_lp(
-                        idx=i, x=x, xprev=xprev, uprev=uprev,
-                        d_min=self._d_min, d_max=self._d_max,
-                        param_min=param_min, param_max=param_max,
-                        tol=self._solver_tol, max_iter=self._max_iter,
-                        max=True
+                        idx=i, x=x, xprev=xprev,
+                        uprev=uprev, max=True
                     )
                 else:
-                    sol_min[i] = param_min[i]
-                    sol_max[i] = param_max[i]
+                    sol_min[i] = p_min[i]
+                    sol_max[i] = p_max[i]
 
-            param_min = np.maximum(sol_min, param_min)
-            param_max = np.minimum(sol_max, param_max)
+            param_min = np.maximum(sol_min, p_min)
+            param_max = np.minimum(sol_max, p_max)
         return param_min, param_max
 
 
@@ -122,56 +113,47 @@ class SetMembershipEstimator:
         x: np.ndarray,
         xprev: np.ndarray,
         uprev: np.ndarray,
-        d_min: np.ndarray,
-        d_max: np.ndarray,
-        param_min: np.ndarray,
-        param_max: np.ndarray,
-        tol: float,
-        max_iter: int,
         max: bool
     ) -> float:
-        P = np.zeros((self._nparam, self._nparam))
-        q = np.zeros(self._nparam)
+        P = np.zeros((self._np, self._np))
+        q = np.zeros(self._np)
         if max: q[idx] = -1.0
         else: q[idx] = 1.0
 
         Fd = np.array(self._Fd(xprev, uprev)).flatten()
         Gd = np.array(self._Gd(xprev, uprev))
         G = np.block([[Gd], [-Gd]])
-        h = np.block([x-Fd-d_min, -x+Fd+d_max])
+        h = np.block([x-Fd-self._d_min, -x+Fd+self._d_max])
 
-        param_bd = proxqp_solve_qp(
-            P=P, q=q, G=G, h=h, lb=param_min, ub=param_max,
+        p_bd = proxqp_solve_qp(
+            P=P, q=q, G=G, h=h,
+            lb=self._p_min, ub=self._p_max,
             verbose=False, backend="dense",
-            eps_abs=tol, max_iter=max_iter,
+            eps_abs=self._sol_tol, max_iter=self._max_iter,
         )
         try:
-            return param_bd[idx]
+            return p_bd[idx]
         except TypeError:
-            if max: return param_max[idx]
-            else: return param_min[idx]
+            if max: return self._p_max[idx]
+            else: return self._p_min[idx]
 
 
     def _solve_proj(
         self,
         param: np.ndarray,
-        param_min: np.ndarray,
-        param_max: np.ndarray,
-        tol: float,
-        max_iter: int
     ) -> np.ndarray:
-        P = np.eye(self._nparam)
-        q = np.zeros(self._nparam)
-        lb = param_min-param
-        ub = param_max-param
-        param_err = proxqp_solve_qp(
+        P = np.eye(self._np)
+        q = np.zeros(self._np)
+        lb = self._p_min - param
+        ub = self._p_max - param
+        p_err = proxqp_solve_qp(
             P=P, q=q, lb=lb, ub=ub,
             verbose=False, backend="dense",
-            eps_abs=tol, max_iter=max_iter,
+            eps_abs=self._sol_tol, max_iter=self._max_iter,
         )
         try:
-            param_proj = param_err + param
-            return param_proj
+            p_proj = p_err + param
+            return p_proj
         except TypeError:
             return param
 
@@ -181,17 +163,18 @@ class SetMembershipEstimator:
         param_min: np.ndarray,
         param_max: np.ndarray,
     ) -> None:
-        self._param_min = param_min
-        self._param_max = param_max
+        self._p_min = param_min
+        self._p_max = param_max
 
 
-class LeastMeanSquareEstimator():
+class LMS():
     def __init__(
         self,
         model: Quadrotor,
         update_gain: float,
         time_step: float,
     ) -> None:
+        self._nx = model.nx
         model_aug = ParameterAffineQuadrotor(model)
         self._Fd, self._Gd, self._Gd_T = \
             self._get_discrete_dynamics(model_aug, self._nx, time_step)
@@ -222,12 +205,12 @@ class LeastMeanSquareEstimator():
     ) -> np.ndarray:
         st = time.perf_counter()
         x_err = x - self._Fd(xprev, uprev) - self._Gd(xprev, uprev)@param
-        param_lms = param + self._mu*self._Gd_T(xprev, uprev)@x_err
-        param_lms = np.array(param_lms).flatten()
+        p_lms = param + self._mu*self._Gd_T(xprev, uprev)@x_err
+        p_lms = np.array(p_lms).flatten()
         if timer:
             et = time.perf_counter()
             print(f"LMS runtime: {et - st}")
-        return param_lms
+        return p_lms
 
 
 class LinearMHE():
