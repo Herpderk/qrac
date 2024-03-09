97ee3a1b2b88be2f7528a3ea4723f3a3cfc3d6c7
Derek Fan <derekfan.18.2002@gmail.com>
2024-03-08 18:07:07 -0800

    added linear mhe

--- qrac/estimation.py
+++ qrac/estimation.py
@@ -1,9 +1,14 @@
+#!/usr/bin/python3
+
+from acados_template import AcadosOcpSolver, AcadosOcp, AcadosModel
 import casadi as cs
 import numpy as np
+from scipy.linalg import block_diag
 from qpsolvers.solvers.proxqp_ import proxqp_solve_qp
-from typing import Tuple
 import time
-from qrac.models import Quadrotor, ParameterAffineQuadrotor
+from typing import Tuple
+from qrac.models import Quadrotor, AffineQuadrotor
+from qrac.control.nmpc import NMPC
 
 
 class SetMembershipEstimator:
@@ -22,7 +27,7 @@ class SetMembershipEstimator:
     ) -> None:
         self._nx = model.nx
         self._est = estimator
-        model_aug = ParameterAffineQuadrotor(model)
+        model_aug = AffineQuadrotor(model)
         self._Fd, self._Gd, self._Gd_T = \
             self._get_discrete_dynamics(model_aug, self._nx, time_step)
 
@@ -33,6 +38,7 @@ class SetMembershipEstimator:
         self._sol_tol = qp_tol
         self._max_iter = max_iter
 
+        self._x = np.zeros(self._nx)
         self._p_min = param_min
         self._p_max = param_max
         self._start = False
@@ -40,7 +46,7 @@ class SetMembershipEstimator:
 
     def _get_discrete_dynamics(
         self,
-        model: ParameterAffineQuadrotor,
+        model: AffineQuadrotor,
         nx: int,
         dt: float
     ) -> Tuple[cs.Function, cs.Function, cs.Function]:
@@ -55,32 +61,34 @@ class SetMembershipEstimator:
     def get_param(
         self,
         x: np.ndarray,
-        xprev: np.ndarray,
-        uprev: np.ndarray,
+        u: np.ndarray,
         param: np.ndarray,
-        timer=False
+        timer=True
     ) -> np.ndarray:
         st = time.perf_counter()
-        p_min, p_max = self._sm_update(x, xprev, uprev)
+        p_min, p_max = self._sm_update(x, u)
         self._update_param_bds(p_min, p_max)
-        p_est = self._est.get_param(x, xprev, uprev, param, False)
-        p_proj = self._solve_proj(param=p_est)
-        print(f"params: {p_proj}")
+        p = self._est.get_param(
+            x=x, u=u, param=param,
+            param_min=p_min, param_max=p_max,
+            timer=False
+        )
+        self._x = x
+        print(f"params: {p}\n")
         if timer:
             et = time.perf_counter()
             print(f"sm runtime: {et - st}")
-        return p_proj
+        return p
 
 
     def _sm_update(
         self,
         x: np.ndarray,
-        xprev: np.ndarray,
-        uprev: np.ndarray
+        u: np.ndarray
     ) -> Tuple[np.ndarray, np.ndarray]:
         if self._start == False:
             self._start = True
-            xprev = x
+            self._x = x
 
         p_min = self._p_min
         p_max = self._p_max
@@ -91,12 +99,12 @@ class SetMembershipEstimator:
             for i in range(self._np):
                 if p_max[i] - p_min[i] > self._p_tol[i]:
                     sol_min[i] = self._solve_lp(
-                        idx=i, x=x, xprev=xprev,
-                        uprev=uprev, max=False
+                        idx=i, x=x, xprev=self._x,
+                        u=u, max=False
                     )
                     sol_max[i] = self._solve_lp(
-                        idx=i, x=x, xprev=xprev,
-                        uprev=uprev, max=True
+                        idx=i, x=x, xprev=self._x,
+                        u=u, max=True
                     )
                 else:
                     sol_min[i] = p_min[i]
@@ -112,7 +120,7 @@ class SetMembershipEstimator:
         idx: int,
         x: np.ndarray,
         xprev: np.ndarray,
-        uprev: np.ndarray,
+        u: np.ndarray,
         max: bool
     ) -> float:
         P = np.zeros((self._np, self._np))
@@ -120,8 +128,8 @@ class SetMembershipEstimator:
         if max: q[idx] = -1.0
         else: q[idx] = 1.0
 
-        Fd = np.array(self._Fd(xprev, uprev)).flatten()
-        Gd = np.array(self._Gd(xprev, uprev))
+        Fd = np.array(self._Fd(xprev, u)).flatten()
+        Gd = np.array(self._Gd(xprev, u))
         G = np.block([[Gd], [-Gd]])
         h = np.block([x-Fd-self._d_min, -x+Fd+self._d_max])
 
@@ -140,26 +148,6 @@ class SetMembershipEstimator:
             else: return self._p_min[idx]
 
 
-    def _solve_proj(
-        self,
-        param: np.ndarray,
-    ) -> np.ndarray:
-        P = np.eye(self._np)
-        q = np.zeros(self._np)
-        lb = self._p_min - param
-        ub = self._p_max - param
-        p_err = proxqp_solve_qp(
-            P=P, q=q, lb=lb, ub=ub,
-            verbose=False, backend="dense",
-            eps_abs=self._sol_tol, max_iter=self._max_iter,
-        )
-        try:
-            p_proj = p_err + param
-            return p_proj
-        except TypeError:
-            return param
-
-
     def _update_param_bds(
         self,
         param_min: np.ndarray,
@@ -175,17 +163,24 @@ class LMS():
         model: Quadrotor,
         update_gain: float,
         time_step: float,
+        qp_tol=10**-6,
+        max_iter=10,
     ) -> None:
         self._nx = model.nx
-        model_aug = ParameterAffineQuadrotor(model)
+        model_aug = AffineQuadrotor(model)
         self._Fd, self._Gd, self._Gd_T = \
             self._get_discrete_dynamics(model_aug, self._nx, time_step)
+
+        self._np = model_aug.np
         self._mu = update_gain
+        self._sol_tol = qp_tol
+        self._max_iter = max_iter
+        self._x = np.zeros(self._nx)
 
 
     def _get_discrete_dynamics(
         self,
-        model: ParameterAffineQuadrotor,
+        model: AffineQuadrotor,
         nx: int,
         dt: float
     ) -> Tuple[cs.Function, cs.Function, cs.Function]:
@@ -200,24 +195,312 @@ class LMS():
     def get_param(
         self,
         x: np.ndarray,
-        xprev: np.ndarray,
-        uprev: np.ndarray,
+        u: np.ndarray,
         param: np.ndarray,
-        timer: bool
+        param_min: np.ndarray,
+        param_max: np.ndarray,
+        timer=True
     ) -> np.ndarray:
-        st = time.perf_counter()
-        x_err = x - self._Fd(xprev, uprev) - self._Gd(xprev, uprev)@param
-        p_lms = param + self._mu*self._Gd_T(xprev, uprev)@x_err
+        if timer: st = time.perf_counter()
+        x_err = x - self._Fd(self._x, u) - self._Gd(self._x, u)@param
+        p_lms = param + self._mu*self._Gd_T(self._x, u)@x_err
         p_lms = np.array(p_lms).flatten()
+        p_proj = self._solve_proj(
+            p=p_lms, p_min=param_min, p_max=param_max
+        )
+        self._x = x
         if timer:
             et = time.perf_counter()
             print(f"LMS runtime: {et - st}")
-        return p_lms
+        return p_proj
+
+
+    def _solve_proj(
+        self,
+        p: np.ndarray,
+        p_min: np.ndarray,
+        p_max: np.ndarray
+    ) -> np.ndarray:
+        P = np.eye(self._np)
+        q = np.zeros(self._np)
+        lb = p_min - p
+        ub = p_max - p
+        p_err = proxqp_solve_qp(
+            P=P, q=q, lb=lb, ub=ub,
+            verbose=False, backend="dense",
+            eps_abs=self._sol_tol, max_iter=self._max_iter,
+        )
+        try:
+            p_proj = p_err + p
+            return p_proj
+        except TypeError:
+            return p
 
 
 class LinearMHE():
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self,
+        model: Quadrotor,
+        Q: np.ndarray,
+        R: np.ndarray,
+        param_min: np.ndarray,
+        param_max: np.ndarray,
+        disturb_min: np.ndarray,
+        disturb_max: np.ndarray,
+        time_step: float,
+        num_nodes: int,
+        rti: bool,
+        nlp_tol=10**-6,
+        nlp_max_iter=10,
+        qp_max_iter=10,
+    ) -> None:
+        """
+        Q -> weight for params
+        R -> weight for disturb
+        """
+        self._nx = model.nx
+        self._nu = model.nu
+        self._dt = time_step
+        self._N = num_nodes
+        self._p_min = param_min
+        self._p_max = param_max
+        self._d_min = disturb_min
+        self._d_max = disturb_max
+        self._d_avg = (disturb_min + disturb_max) / 2
+
+        model_aug = AffineQuadrotor(model)
+        model_aug.set_prediction_model()
+        self._np = model_aug.np
+        Q_aug = self._augment_costs(Q)
+        self._solver = self._init_solver(
+            model=model_aug, Q=Q_aug, R=R,
+            p_min=param_min, p_max=param_max,
+            d_min=disturb_min, d_max=disturb_max, rti=rti,
+            nlp_tol=nlp_tol, nlp_max_iter=nlp_max_iter,
+            qp_max_iter=qp_max_iter
+        )
+
+        self._x = np.zeros((num_nodes, self._nx))
+        self._u = np.zeros((num_nodes-1, self._nu))
+        self._d = np.zeros((num_nodes-1, self._nx))
+
+
+    def get_param(
+        self,
+        x: np.ndarray,
+        u: np.ndarray,
+        param: np.ndarray,
+        param_min=np.array([]),
+        param_max=np.array([]),
+        timer=True
+    ) -> np.ndarray:
+        self._solve(
+            x=x, u=u, p=param, p_min=param_min,
+            p_max=param_max, timer=timer
+        )
+        p_mhe = np.array(
+            self._solver.get(1,"x")[self._nx : self._nx+self._np]
+        )
+        return p_mhe
+
+
+    def _solve(
+        self,
+        x: np.ndarray,
+        u: np.ndarray,
+        p: np.ndarray,
+        timer: bool,
+        p_min: np.ndarray,
+        p_max: np.ndarray,
+    ) -> np.ndarray:
+        if timer: st = time.perf_counter()
+        assert x.shape[0] == self._nx
+        assert u.shape[0] == self._nu
+        assert p.shape[0] == self._np
+        #assert p_min.shape[0] == self._np
+        #assert p_max.shape[0] == self._np
+        
+        # set history of x, u, and d at each stage
+        for k in range(self._N - 1):
+            self._set_stage(
+                k=k, x=self._x[k+1], u=self._u[k], d=self._d[k],
+                p=p, p_min=p_min, p_max=p_max
+            )
+        # set new measurements
+        self._set_stage(
+            k=self._N, x=x, u=u,
+            p=p, p_min=p_min, p_max=p_max
+        )
+        self._solver.solve()
+
+        # get the latest disturbance estimate
+        # propagate the horizon by 1 step
+        d = np.array(self._solver.get(self._N-1, "u"))
+        self._update_horizon(x=x, u=u, d=d)
+
+        if timer:
+            et = time.perf_counter()
+            print(f"mhe runtime: {et - st}")
+
+
+    def _set_stage(
+        self,
+        k: int,
+        x: np.ndarray,
+        u: np.ndarray,
+        p: np.ndarray,
+        p_min: np.ndarray,
+        p_max: np.ndarray,
+        d=np.array([]),
+    ):
+        x_aug = np.concatenate((x, p))
+        self._solver.set(k, "x", x_aug)
+        self._solver.set(k, "p", u)
+
+        if k != self._N:
+            yref = np.concatenate((x_aug, d))
+            self._solver.set(k, "yref", yref)
+            self._solver.set(k, "u", d)
+
+        if len(p_min) == 0:
+            p_min = self._p_min
+        if k == 0:
+            p_min = np.concatenate((x, p_min))
+        self._solver.set(k, "lbx", p_min)
+
+        if len(p_max) == 0:
+            p_max = self._p_max
+        if k == 0:
+            p_max = np.concatenate((x, p_max))
+        self._solver.set(k, "ubx", p_max)
+
+
+
+    def _update_horizon(
+        self,
+        x: np.ndarray,
+        u: np.ndarray,
+        d: np.ndarray
+    ):
+        '''
+        for k in range(self._N):
+            self._x[k] = np.array(
+                self._solver.get(k+1, "x")[:self._nx]
+            )
+        for k in range(self._N-1):
+            self._d[k] = np.array(
+                self._solver.get(k+1, "u")
+        )'''
+        self._x = np.block([
+            [self._x[1:self._N, :]], [x]
+        ])
+        self._u = np.block([
+            [self._u[1:self._N, :]], [u]
+        ])
+        self._d = np.block([
+            [self._d[1:self._N, :]], [d]
+        ])    
+
+
+    def _init_solver(
+        self,
+        model: Quadrotor,
+        Q: np.ndarray,
+        R: np.ndarray,
+        p_min: np.ndarray,
+        p_max: np.ndarray,
+        d_min: np.ndarray,
+        d_max: np.ndarray,
+        rti: bool,
+        nlp_tol: float,
+        nlp_max_iter: int,
+        qp_max_iter: int
+    ) -> AcadosOcpSolver:
+        ny = model.nx + model.nu  # combine x and u into y
+
+        ocp = AcadosOcp()
+        ocp.model = model.get_acados_model()
+        ocp.dims.N = self._N
+        ocp.dims.nx = model.nx
+        ocp.dims.nu = model.nu
+        ocp.dims.ny = ny
+        ocp.dims.nbx = model.np
+        ocp.dims.nbx_0 = ocp.dims.nbx
+        ocp.dims.nbx_e = ocp.dims.nbx
+        ocp.dims.nbu = model.nu
+
+        # total horizon in seconds
+        ocp.solver_options.tf = self._dt * self._N
+
+        # formulate the default least-squares cost as a quadratic cost
+        ocp.cost.cost_type = "LINEAR_LS"
+        ocp.cost.cost_type_e = "LINEAR_LS"
+
+        # W is a block diag matrix of Q and R costs from standard QP
+        ocp.cost.W = block_diag(Q,R)
+
+        # use V coeffs to map x & u to y
+        ocp.cost.Vx = np.zeros((ny, model.nx))
+        ocp.cost.Vx[: model.nx, : model.nx] = np.eye(model.nx)
+        ocp.cost.Vu = np.zeros((ny, model.nu))
+        ocp.cost.Vu[-model.nu :, -model.nu :] = np.eye(model.nu)
+        ocp.cost.Vu_0 = ocp.cost.Vu
+
+        # init reference trajectory (will be overwritten)
+        ocp.cost.yref = np.zeros(ny)
+
+        # init parameter vector
+        ocp.parameter_values = np.zeros(self._nu)
+
+        # control input constraints (disturbance)
+        ocp.constraints.idxbu = np.arange(model.nu)
+        ocp.constraints.lbu = d_min
+        ocp.constraints.ubu = d_max
+        
+        # Initial state (will be overwritten)
+        ocp.constraints.x0 = np.zeros(model.nx)
+
+        # augmented state
+        ocp.constraints.idxbx = np.arange(model.np)
+        ocp.constraints.lbx = p_min
+        ocp.constraints.ubx = p_max
+        ocp.constraints.idxbx_e = np.arange(model.np)
+        ocp.constraints.lbx_e = p_min
+        ocp.constraints.ubx_e = p_max
+
+        # partial condensing HPIPM is fastest:
+        # https://cdn.syscop.de/publications/Frison2020a.pdf
+        ocp.solver_options.hpipm_mode = "SPEED_ABS"
+        ocp.solver_options.qp_solver = "PARTIAL_CONDENSING_HPIPM"
+        ocp.solver_options.qp_solver_warm_start = 1
+        ocp.solver_options.qp_solver_iter_max = qp_max_iter
+        ocp.solver_options.nlp_solver_max_iter = nlp_max_iter
+        ocp.solver_options.nlp_solver_tol_stat = nlp_tol
+        ocp.solver_options.hessian_approx = "GAUSS_NEWTON"
+        ocp.solver_options.integrator_type = "ERK"
+        ocp.solver_options.print_level = 0
+
+        ocp.code_export_directory = "linear_mhe_c_code"
+        name = "acados_linear_mhe.json"
+
+        if rti:
+            ocp.solver_options.nlp_solver_type = "SQP_RTI"
+            solver = AcadosOcpSolver(ocp, json_file=name)
+            solver.options_set("rti_phase", 0)
+        else:
+            ocp.solver_options.nlp_solver_type = "SQP"
+            solver = AcadosOcpSolver(ocp, json_file=name)
+        return solver
+
+
+    def _augment_costs(
+        self,
+        Q: np.ndarray
+    ) -> np.ndarray:
+        Q_aug = block_diag(
+            (10**10)*np.eye(self._nx), Q
+        )
+        return Q_aug
 
 
 class NonlinearMHE():
