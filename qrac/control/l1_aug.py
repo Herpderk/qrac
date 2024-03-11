09b2a18092cd38bd892cee44112d880bb3b6ab74
Derek Fan <derekfan.18.2002@gmail.com>
2024-03-11 00:56:24 -0700

    removed multiprocessing

--- qrac/control/l1_aug.py
+++ qrac/control/l1_aug.py
@@ -16,14 +16,12 @@ class L1Augmentation():
         control_ref,
         adapt_gain: float,
         bandwidth: float,
-        time_step: float,
-        real_time: bool,
     ) -> None:
         self._z_idx = 6
         self._nz = 6
         self._nm = 4
         self._num = 2
-        self._assert(model, control_ref, adapt_gain, bandwidth, time_step, real_time)
+        self._assert(model, control_ref, adapt_gain, bandwidth,)
 
         self._u_l1 = np.zeros(model.nu)
         self._z = np.zeros(self._nz)
@@ -40,21 +38,15 @@ class L1Augmentation():
         ])
         self._adapt_gain = adapt_gain
         self._adapt_exp, self._adapt_mat, self._filter_exp = \
-            self._get_l1_const(bandwidth, time_step)
+            self._get_l1_const(bandwidth, control_ref.dt)
 
         self._model = model
         self._ctrl_ref = control_ref
-        self._dt = time_step
-        self._rt = real_time
-        self._loop_ratio = int(round(control_ref.dt / time_step))
-        self._loop_ct = int(round(control_ref.dt / time_step))
+        self._dt = control_ref.dt
 
-        self._x = mp.Array("f", np.zeros(model.nx))
-        self._xset = mp.Array("f", np.zeros(control_ref.n_set))
-        self._u_ref = mp.Array("f", np.zeros(model.nu))
-        self._timer = mp.Value("b", False)
-        if real_time:
-            self._run_flag = mp.Value("b", True)
+        self._x = np.zeros(model.nx)
+        self._xset = np.zeros(control_ref.n_set)
+        self._u_ref = np.zeros(model.nu)
 
 
     @property
@@ -67,21 +59,6 @@ class L1Augmentation():
         return self._ctrl_ref.n_set
 
 
-    def start(self) -> None:
-        if not self._rt:
-            print("This controller is not in real-time mode!")
-        else:
-            proc = mp.Process(target=self._ctrl_ref_proc, args=[])
-            proc.start()
-            
-
-    def stop(self) -> None:
-        if not self._rt:
-            print("This controller is not in real-time mode!")
-        else:
-            self._run_flag.value = False
-
-
     def get_input(
         self,
         x: np.ndarray,
@@ -90,94 +67,69 @@ class L1Augmentation():
     ) -> np.ndarray:
         assert x.shape[0] == self._model.nx
         assert xset.shape[0] == self._ctrl_ref.n_set
-        self._timer.value = timer
-        self._x[:] = x
-        self._xset[:] = xset
-        if not self._rt:
-            self._update_non_rt()
-        self._run_l1()
-        u = np.array(self._u_ref[:]) + self._u_l1
+        uref = self._ctrl_ref.get_input(x=x, xset=xset, timer=timer)
+        ul1 = self._get_l1_input(x=x, uref=uref, timer=timer)
+        u = uref + ul1
         return u
 
 
-    def _ctrl_ref_proc(self) -> None:
-        ctrl_ref_proc = mp.Process(target=self._run_ctrl_ref, args=[])
-        ctrl_ref_proc.start()
-        ctrl_ref_proc.join()
-        print("\nController successfully stopped.")
-
-
-    def _run_ctrl_ref(self) -> None:
+    def _get_l1_input(
+        self,
+        x: np.ndarray,
+        uref: np.ndarray,
+        timer: bool
+    ) -> None:
         st = time.perf_counter()
-        while self._run_flag.value:
-            et = time.perf_counter()
-            if et - st >= self._ctrl_ref.dt:
-                st = et
-                self._update_ctrl_ref()
-
-
-    def _update_non_rt(self) -> None:
-        if self._loop_ct == self._loop_ratio:
-            self._loop_ct = 0
-            self._update_ctrl_ref()
-        self._loop_ct += 1
-
-
-    def _update_ctrl_ref(self) -> np.ndarray:
-        x = np.array(self._x[:])
-        xset = np.array(self._xset[:])
-        timer = self._timer.value
-        self._u_ref[:] = self._ctrl_ref.get_input(
-            x=x, xset=xset, timer=timer
-        )
-
-
-    def _run_l1(self) -> None:
-        st = time.perf_counter()
-        self._predictor()
-        self._adaptation()
-        self._control_law()
-        if self._timer.value:
+        z_err, g_m, g_um = self._predictor(x=x, uref=uref)
+        dstb_m = self._adaptation(z_err=z_err, g_m=g_m, g_um=g_um)
+        ul1 = self._control_law(dstb_m=dstb_m)
+        if timer:
             print(f"L1 runtime: {time.perf_counter() - st}")
-        if self._rt:
-            while time.perf_counter() - st < self._dt:
-                pass
+        return ul1
 
 
-    def _predictor(self) -> None:
-        x = np.array(self._x[:])
-        u_ref = np.array(self._u_ref[:])
-        z = np.array(self._z[:])
-        z_err = np.array(z - x[self._z_idx : self._z_idx+self._nz])
-        f = np.array(self._f(x, u_ref)).reshape(self._nz)
+    def _predictor(
+        self,
+        x: np.ndarray,
+        uref: np.ndarray
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        z_err = np.array(
+            self._z - x[self._z_idx : self._z_idx+self._nz]
+        )
+        f = np.array(self._f(x, uref)).reshape(self._nz)
+
         g_m = np.array(self._g_m(x))
         g_um = np.array(self._g_um(x))
-        u_l1 = self._u_l1
-        dstb_m = self._dstb_m
-        dstb_um = self._dstb_um
-        z += + self._dt * \
-            (f + g_m@(u_l1+dstb_m) + g_um@dstb_um + self._Am@z_err)
-        self._z[:] = z
+        ul1 = self._u_l1
+        d_m = self._dstb_m
+        d_um = self._dstb_um
+        self._z += self._dt * (self._Am@z_err + \
+            f + g_m@(ul1+d_m) + g_um@d_um)
+        return z_err, g_m, g_um
 
 
-    def _adaptation(self) -> None:
-        x = np.array(self._x[:])
-        z = np.array(self._z[:])
-        z_err = np.array(z - x[self._z_idx : self._z_idx+self._nz])
-        g_m = np.array(self._g_m(x))
-        g_um = np.array(self._g_um(x))
-
+    def _adaptation(
+        self,
+        z_err,
+        g_m: np.ndarray,
+        g_um: np.ndarray
+    ) -> np.ndarray:
         I = np.eye(self._nz)
         G = np.block([g_m, g_um])
         mu = self._adapt_exp @ z_err
         adapt = self._adapt_gain * -I @ np.linalg.inv(G) @ self._adapt_mat @ mu
         self._dstb_m = adapt[: self._nm]
         self._dstb_um = adapt[self._nm : self._nm + self._num]
+        return self._dstb_m
 
 
-    def _control_law(self) -> None:
+    def _control_law(
+        self,
+        dstb_m: np.ndarray
+    ) -> None:
         self._u_l1 = self._filter_exp*self._u_l1 - \
-            (1-self._filter_exp)*self._dstb_m
+            (1-self._filter_exp)*dstb_m
+        return self._u_l1
 
 
     def _get_l1_const(
@@ -222,8 +174,6 @@ class L1Augmentation():
         control_ref,
         adapt_gain: float,
         bandwidth: float,
-        time_step: float,
-        real_time: bool,
     ) -> None:
         if type(model) != Quadrotor:
             raise TypeError(
@@ -250,12 +200,3 @@ class L1Augmentation():
         if type(bandwidth) != int and type(bandwidth) != float:
             raise TypeError(
                 "Please input the cutoff frequency as an integer or float!")
-        if type(time_step) != int and type(time_step) != float:
-            raise ValueError(
-                "Please input the control loop step as an integer or float!")
-        if type(real_time) != bool:
-            raise ValueError(
-                "Please input the real-time mode as a bool!")
-        if control_ref.dt < time_step:
-            raise ValueError(
-                "Please make sure the reference controller time step is greater than or equal to the L1 time step!")
