e2dcfca78143cd5dd5caca015834fcae1cd1d25f
Derek Fan <derekfan.18.2002@gmail.com>
2024-03-18 21:52:16 -0700

    added lemniscate trajectory saving and loading

---
+++ examples/lemniscate/l1_nmpc.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+
+import numpy as np
+from qrac.models import Quadrotor, Crazyflie
+from qrac.control import NMPC
+from qrac.control import L1Augmentation
+from qrac.sim import MinimalSim
+
+
+def run():
+    # mpc settings
+    CTRL_T = 0.01
+    NODES = 150
+    Q = np.diag([1,1,1, 1,1,1, 1,1,1, 1,1,1,])
+    R = np.diag([0, 0, 0, 0])
+
+    # L1 settings
+    A_GAIN = 80
+    W = 1000
+
+    # sim settings
+    SIM_T = CTRL_T / 10
+    D_MAX = np.array([
+        0,0,0, 0,0,0, 10,10,10, 10,10,10,
+    ])
+
+
+    # load in time optimal trajectory
+    xref = np.load("refs/xref.npy")
+    uref = np.load("refs/uref.npy")
+
+
+    # inaccurate model
+    inacc = Crazyflie(Ax=0, Ay=0, Az=0)
+
+    # true model
+    m_true = 1.5 * inacc.m
+    Ixx_true = 1.8 * inacc.Ixx
+    Iyy_true = 1.8 * inacc.Iyy
+    Izz_true = 1.8 * inacc.Izz
+    Ax_true = 0
+    Ay_true = 0
+    Az_true = 0
+    xB_true =inacc.xB
+    yB_true =inacc.yB
+    k_true =inacc.k
+    u_min_true =inacc.u_min
+    u_max_true =inacc.u_max
+    acc = Quadrotor(
+        m_true, Ixx_true, Iyy_true, Izz_true,
+        Ax_true, Ay_true, Az_true,
+        xB_true, yB_true, k_true,
+        u_min_true, u_max_true)
+
+
+    # init mpc
+    mpc = NMPC(
+        model=inacc, Q=Q, R=R,
+        u_min=inacc.u_min, u_max=inacc.u_max, 
+        time_step=CTRL_T, num_nodes=NODES,
+        rti=True, nlp_max_iter=1, qp_max_iter=5
+    )
+
+    # init L1
+    l1_mpc = L1Augmentation(
+        model=inacc, control_ref=mpc,
+        adapt_gain=A_GAIN, bandwidth=W,
+    )
+
+
+    # init sim
+    steps = xref.shape[0]
+    sim = MinimalSim(
+        model=acc, data_len=steps,
+        sim_step=SIM_T, control_step=CTRL_T,
+    )
+
+
+    # run for predefined number of steps
+    nx = inacc.nx
+    xset = np.zeros(nx*NODES)
+    x = xref[0]
+    nu = inacc.nu
+    uset = np.zeros(nu*NODES)
+
+    for k in range(steps):
+        diff = steps - k
+        if diff < NODES:
+            xset[:nx*diff] = xref[k : k+diff, :].flatten()
+            xset[:nx*(NODES-diff)] = np.tile(xref[-1,:], NODES-diff)
+            uset[:nu*diff] = uref[k : k+diff, :].flatten()
+            uset[:nu*(NODES-diff)] = np.tile(uref[-1,:], NODES-diff)
+
+        else:
+            xset[:] = xref[k : k+NODES, :].flatten()
+            uset[:nu*NODES] = uref[k : k+NODES, :].flatten()
+
+        u = l1_mpc.get_input(x=x, xset=xset, uset=uset, timer=True)
+        d = 2*D_MAX*(-0.5 + np.random.rand(12))
+        x = sim.update(x=x, u=u, d=d, timer=True)
+
+        print(f"\nu: {u}")
+        print(f"x: {x}")
+        print(f"sim time: {(k+1)*CTRL_T}\n")
+
+
+    # plot
+    sim.get_animation(
+        filename="/home/derek/Documents/qrac/l1_exp.gif"
+    )
+
+
+if __name__=="__main__":
+    run()
---
+++ examples/lemniscate/refs/ref_gen.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+from qrac.models import Crazyflie
+from qrac.trajectory import LemniScate
+from qrac.control import NMPC
+import numpy as np
+
+
+def run():
+    # nmpc settings
+    PREDICT_TIME = 30
+    CTRL_T = 0.01
+    NODES = int(round(PREDICT_TIME / CTRL_T))
+    Q = np.diag([4,4,4, 1,1,1, 0,0,0, 0,0,0,])
+    R = np.diag([0, 0, 0, 0])
+
+
+    # traj settings
+    A = 5
+    B = 1
+    AXES = [0,1]
+    TRANSLATE = [0,0,5]
+    xfilename = "xref.npy"
+    ufilename = "uref.npy"
+
+
+    # init model
+    model = Crazyflie(Ax=0, Ay=0, Az=0)
+
+
+    # init nmpc
+    nmpc = NMPC(
+        model=model, Q=Q, R=R,
+        u_min=model.u_min, u_max=model.u_max, 
+        time_step=CTRL_T, num_nodes=NODES,
+        rti=False, nlp_max_iter=100, qp_max_iter=50
+    )
+
+
+    # define a trajectory
+    traj = LemniScate(
+        a=A, b=B, axes=AXES, translation=TRANSLATE
+    )
+
+
+    # run for predefined number of steps
+    nx = model.nx
+    x = np.zeros(nx)
+    xset = np.zeros(nx*NODES)
+
+    for k in range(NODES):
+        xset[k*nx : k*nx + nx] = traj.get_setpoint(k*CTRL_T)
+    xref, uref = nmpc.get_trajectory(x=x, xset=xset, timer=True, visuals=True)
+
+
+    # output results
+    vavg = np.average(
+        np.linalg.norm(xref[:, 6:9], axis=1)
+    )
+    print(f"Avg velocity: {vavg}")
+    np.save(xfilename, xref)
+    np.save(ufilename, uref)
+
+
+if __name__=="__main__":
+    run()
