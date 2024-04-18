1c2a36eb625770bacd6e6e44d3432bb881fcaaa2
Derek Fan <herpderkfanie@gmail.com>
2024-04-18 01:25:09 -0700

    streamlined startup

--- qrac/interface.py
+++ qrac/interface.py
@@ -36,10 +36,12 @@ class OptiTrackInterface():
         command_port=1510,
         data_port=1511,
         use_multicast=True,
+        verbose=False,
     ):
         self._id = frame_id
-        self._pos = np.zeros(3)
-        self._q = np.zeros(4)
+        self._verbose = verbose
+        self._pos = ()
+        self._q = ()
 
         self._client = NatNetClient(
             rigid_body_cb=self._rigid_body_cb,
@@ -59,20 +61,23 @@ class OptiTrackInterface():
         self._client.shutdown()
 
     def get_position(self):
-        return self._pos
+        return np.array(self._pos)
 
-    def get_quaternion(self):
-        return self._q
+    def get_orientation(self):
+        return np.array(self._q)
 
     # This is a callback function that gets connected to the NatNet client. It is called once per rigid body per frame
     def _rigid_body_cb(self, frame_id, pos, quat):
         if frame_id != self._id:
             pass
         else:
-            self._pos[:] = pos
-            self._q[:] = quat
-            print( "Received frame for rigid body", frame_id )
-            print( "Received frame for rigid body", frame_id," ",pos," ",quat)
+            self._pos = pos
+            self._q = quat
+
+            if self._verbose:
+                print( "Received frame for rigid body", frame_id )
+                print(f"pos: {pos}")
+                print(f"quat: {quat}\n")
 
     # This is a callback function that gets connected to the NatNet client
     # and called once per mocap frame.
@@ -102,8 +107,6 @@ class OptiTrackInterface():
             finally:
                 print("exiting")
 
-        #is_looping = True
-        is_looping = False
         time.sleep(1)
         if self._client.connected() is False:
             print("ERROR: Could not connect properly.  Check that Motive streaming is on.")
@@ -117,102 +120,6 @@ class OptiTrackInterface():
         print_configuration(self._client)
         print("\n")
         print_commands(self._client.can_change_bitstream_version())
-
-
-        while is_looping:
-            inchars = input('Enter command or (\'h\' for list of commands)\n')
-            if len(inchars)>0:
-                c1 = inchars[0].lower()
-                if c1 == 'h' :
-                    print_commands(self._client.can_change_bitstream_version())
-                elif c1 == 'c' :
-                    print_configuration(self._client)
-                elif c1 == 's':
-                    request_data_descriptions(self._client)
-                    time.sleep(1)
-                elif (c1 == '3') or (c1 == '4'):
-                    if self._client.can_change_bitstream_version():
-                        tmp_major = 4
-                        tmp_minor = 1
-                        if(c1 == '3'):
-                            tmp_major = 3
-                            tmp_minor = 1
-                        return_code = self._client.set_nat_net_version(tmp_major,tmp_minor)
-                        time.sleep(1)
-                        if return_code == -1:
-                            print("Could not change bitstream version to %d.%d"%(tmp_major,tmp_minor))
-                        else:
-                            print("Bitstream version at %d.%d"%(tmp_major,tmp_minor))
-                    else:
-                        print("Can only change bitstream in Unicast Mode")
-
-                elif c1 == 'p':
-                    sz_command="TimelineStop"
-                    return_code = self._client.send_command(sz_command)
-                    time.sleep(1)
-                    print("Command: %s - return_code: %d"% (sz_command, return_code) )
-                elif c1 == 'r':
-                    sz_command="TimelinePlay"
-                    return_code = self._client.send_command(sz_command)
-                    print("Command: %s - return_code: %d"% (sz_command, return_code) )
-                elif c1 == 'o':
-                    tmpCommands=["TimelinePlay",
-                                "TimelineStop",
-                                "SetPlaybackStartFrame,0",
-                                "SetPlaybackStopFrame,1000000",
-                                "SetPlaybackLooping,0",
-                                "SetPlaybackCurrentFrame,0",
-                                "TimelineStop"]
-                    for sz_command in tmpCommands:
-                        return_code = self._client.send_command(sz_command)
-                        print("Command: %s - return_code: %d"% (sz_command, return_code) )
-                    time.sleep(1)
-                elif c1 == 'w':
-                    tmp_commands=["TimelinePlay",
-                                "TimelineStop",
-                                "SetPlaybackStartFrame,1",
-                                "SetPlaybackStopFrame,1500",
-                                "SetPlaybackLooping,0",
-                                "SetPlaybackCurrentFrame,100",
-                                "TimelineStop"]
-                    for sz_command in tmp_commands:
-                        return_code = self._client.send_command(sz_command)
-                        print("Command: %s - return_code: %d"% (sz_command, return_code) )
-                    time.sleep(1)
-                elif c1 == 't':
-                    test_classes()
-
-                elif c1 == 'j':
-                    self._client.set_print_level(0)
-                    print("Showing only received frame numbers and supressing data descriptions")
-                elif c1 == 'k':
-                    self._client.set_print_level(1)
-                    print("Showing every received frame")
-
-                elif c1 == 'l':
-                    print_level = self._client.set_print_level(20)
-                    print_level_mod = print_level % 100
-                    if(print_level == 0):
-                        print("Showing only received frame numbers and supressing data descriptions")
-                    elif (print_level == 1):
-                        print("Showing every frame")
-                    elif (print_level_mod == 1):
-                        print("Showing every %dst frame"%print_level)
-                    elif (print_level_mod == 2):
-                        print("Showing every %dnd frame"%print_level)
-                    elif (print_level == 3):
-                        print("Showing every %drd frame"%print_level)
-                    else:
-                        print("Showing every %dth frame"%print_level)
-
-                elif c1 == 'q':
-                    is_looping = False
-                    self._client.shutdown()
-                    break
-                else:
-                    print("Error: Command %s not recognized"%c1)
-                print("Ready...\n")
-        print("exiting")
    
 
 def add_lists(totals, totals_tmp):
